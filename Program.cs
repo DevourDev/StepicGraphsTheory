fcfccd2f5a773883f9c99e65bfb373c75cc98bee
DevourDev <thek8@yandex.ru>
2023-03-26 22:21:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-using GraphsTheory.Lessons;
-
-Console.WriteLine("Hello, World!");
-
-Lesson3.Step1();
+﻿Console.WriteLine("Hello, World!");
